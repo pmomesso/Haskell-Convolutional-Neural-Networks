071ab1b46f0ad95daeb06d62a59341f55d66a617
Pedro Momesso <pmomesso@itba.edu.ar>
2021-12-05 12:05:41 +0000

    ADD: dependency for random number generator

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,14 @@
 module Main where
 
+import qualified Layers as L
+import qualified Data.Matrix as M
+import System.Random
+
 main :: IO ()
 main = do {
     putStrLn "Hola"
 }
+
+randomFloat :: IO Float
+randomFloat = do
+    randomIO :: IO Float
